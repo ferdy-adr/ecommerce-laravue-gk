8fae3208beea6b44098fb313d1716f7a70d46b30
ferdy-adr <ferdypbg@gmail.com>
2021-12-03 13:25:54 +0700

    create component HeroSection

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,67 +1,7 @@
 <template>
   <div class="home">
     <Header />
-
-    <!-- Hero Section Begin -->
-    <section class="hero-section">
-      <carousel class="hero-items" :items="1" :nav="false">
-        <div
-          class="single-hero-items set-bg"
-          style="background-image: url('/img/hero-1.jpg')"
-        >
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-5">
-                <span>Bag,kids</span>
-                <h1>Black friday</h1>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore
-                </p>
-                <a href="#" class="primary-btn">Shop Now</a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div
-          class="single-hero-items set-bg"
-          style="background-image: url('/img/hero-2.jpg')"
-        >
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-5">
-                <span>Bag,kids</span>
-                <h1>Black friday</h1>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore
-                </p>
-                <a href="#" class="primary-btn">Shop Now</a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div
-          class="single-hero-items set-bg"
-          style="background-image: url('/img/hero-3.jpg')"
-        >
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-5">
-                <span>Bag,kids</span>
-                <h1>Black friday</h1>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore
-                </p>
-                <a href="#" class="primary-btn">Shop Now</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </carousel>
-    </section>
-    <!-- Hero Section End -->
+    <HeroSection />
   </div>
 </template>
 
@@ -69,14 +9,14 @@
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
 import Header from "@/components/Header.vue";
-import carousel from "vue-owl-carousel";
+import HeroSection from "@/components/HeroSection.vue";
 
 export default {
   name: "Home",
   components: {
     // HelloWorld,
     Header,
-    carousel,
+    HeroSection,
   },
 };
 </script>
