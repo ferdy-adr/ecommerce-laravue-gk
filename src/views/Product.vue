0ec536fd96c19ecb7047eb1440860553bf164cdb
ferdy-adr <ferdypbg@gmail.com>
2021-12-04 13:37:33 +0700

    create view Product

---
+++ src/views/Product.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="product">
+    <Header />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "Product",
+  components: {
+    // HelloWorld,
+    Header,
+    Footer,
+  },
+};
+</script>
