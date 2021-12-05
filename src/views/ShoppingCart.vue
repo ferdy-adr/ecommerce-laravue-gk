a2e31e3d320b04ac158730425831d50427c8a1e9
ferdy-adr <ferdypbg@gmail.com>
2021-12-05 23:05:21 +0700

    create & route view ShoppingCart

---
+++ src/views/ShoppingCart.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="shopping-cart">
+    <Header />
+
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "ShoppingCart",
+  components: {
+    Header,
+    Footer,
+  },
+};
+</script>
