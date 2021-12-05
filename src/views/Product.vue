be1e563c566cfa6f8599c2ed666279db4e417db0
ferdy-adr <ferdypbg@gmail.com>
2021-12-05 14:54:48 +0700

    add vue-owl-carousel, insert & styling Product Section

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,6 +1,129 @@
 <template>
   <div class="product">
     <Header />
+
+    <!-- Breadcrumb Section Begin -->
+    <div class="breacrumb-section text-left">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="breadcrumb-text product-more">
+              <a href="./home.html"><i class="fa fa-home"></i> Home</a>
+              <span>Detail</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Breadcrumb Section Begin -->
+
+    <!-- Product Shop Section Begin -->
+    <section class="product-shop spad page-details">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="row">
+              <div class="col-lg-6">
+                <div class="product-pic-zoom">
+                  <img
+                    class="product-big-img"
+                    src="img/products/jacket/lyle-scott-jacket-1.jpg"
+                    alt=""
+                  />
+                </div>
+                <div class="product-thumbs">
+                  <carousel
+                    :nav="false"
+                    :dots="false"
+                    class="product-thumbs-track ps-slider"
+                  >
+                    <div
+                      class="pt active"
+                      data-imgbigurl="img/products/jacket/lyle-scott-jacket-1.jpg"
+                    >
+                      <img
+                        src="img/products/jacket/lyle-scott-jacket-1.jpg"
+                        alt=""
+                      />
+                    </div>
+
+                    <div
+                      class="pt"
+                      data-imgbigurl="img/products/jacket/lyle-scott-jacket-2.jpg"
+                    >
+                      <img
+                        src="img/products/jacket/lyle-scott-jacket-2.jpg"
+                        alt=""
+                      />
+                    </div>
+
+                    <div
+                      class="pt"
+                      data-imgbigurl="img/products/jacket/lyle-scott-jacket-3.jpg"
+                    >
+                      <img
+                        src="img/products/jacket/lyle-scott-jacket-3.jpg"
+                        alt=""
+                      />
+                    </div>
+
+                    <div
+                      class="pt"
+                      data-imgbigurl="img/products/jacket/lyle-scott-jacket-4.jpg"
+                    >
+                      <img
+                        src="img/products/jacket/lyle-scott-jacket-4.jpg"
+                        alt=""
+                      />
+                    </div>
+                  </carousel>
+                </div>
+              </div>
+              <div class="col-lg-6">
+                <div class="product-details text-left">
+                  <div class="pd-title">
+                    <span>oranges</span>
+                    <h3>Pure Pineapple</h3>
+                  </div>
+                  <div class="pd-desc">
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Corporis, error officia. Rem aperiam laborum voluptatum
+                      vel, pariatur modi hic provident eum iure natus quos non a
+                      sequi, id accusantium! Autem.
+                    </p>
+                    <p>
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Quam possimus quisquam animi, commodi, nihil voluptate
+                      nostrum neque architecto illo officiis doloremque et
+                      corrupti cupiditate voluptatibus error illum. Commodi
+                      expedita animi nulla aspernatur. Id asperiores blanditiis,
+                      omnis repudiandae iste inventore cum, quam sint molestiae
+                      accusamus voluptates ex tempora illum sit perspiciatis.
+                      Nostrum dolor tenetur amet, illo natus magni veniam quia
+                      sit nihil dolores. Commodi ratione distinctio harum
+                      voluptatum velit facilis voluptas animi non laudantium, id
+                      dolorem atque perferendis enim ducimus? A exercitationem
+                      recusandae aliquam quod. Itaque inventore obcaecati, unde
+                      quam impedit praesentium veritatis quis beatae ea atque
+                      perferendis voluptates velit architecto?
+                    </p>
+                    <h4>$495.00</h4>
+                  </div>
+                  <div class="quantity">
+                    <a href="shopping-cart.html" class="primary-btn pd-cart"
+                      >Add To Cart</a
+                    >
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Product Shop Section End -->
+
     <Footer />
   </div>
 </template>
@@ -9,6 +132,7 @@
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
 import Header from "@/components/Header.vue";
+import carousel from "vue-owl-carousel";
 import Footer from "@/components/Footer.vue";
 
 export default {
@@ -16,7 +140,14 @@ export default {
   components: {
     // HelloWorld,
     Header,
+    carousel,
     Footer,
   },
 };
 </script>
+
+<style scoped>
+.product-thumbs .pt {
+  margin-right: 15px;
+}
+</style>
