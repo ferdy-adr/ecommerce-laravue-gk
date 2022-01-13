4df4144394b7a2d2af5417a5aecb8fc7984fbd9c
ferdy-adr <ferdypbg@gmail.com>
2022-01-13 22:20:05 +0700

    integrate Product by Product.id using Axios

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -82,33 +82,12 @@
               <div class="col-lg-6">
                 <div class="product-details text-left">
                   <div class="pd-title">
-                    <span>oranges</span>
-                    <h3>Pure Pineapple</h3>
+                    <span>{{productDetails.categories_id}}</span>
+                    <h3>{{productDetails.name}}</h3>
                   </div>
                   <div class="pd-desc">
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Corporis, error officia. Rem aperiam laborum voluptatum
-                      vel, pariatur modi hic provident eum iure natus quos non a
-                      sequi, id accusantium! Autem.
-                    </p>
-                    <p>
-                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                      Quam possimus quisquam animi, commodi, nihil voluptate
-                      nostrum neque architecto illo officiis doloremque et
-                      corrupti cupiditate voluptatibus error illum. Commodi
-                      expedita animi nulla aspernatur. Id asperiores blanditiis,
-                      omnis repudiandae iste inventore cum, quam sint molestiae
-                      accusamus voluptates ex tempora illum sit perspiciatis.
-                      Nostrum dolor tenetur amet, illo natus magni veniam quia
-                      sit nihil dolores. Commodi ratione distinctio harum
-                      voluptatum velit facilis voluptas animi non laudantium, id
-                      dolorem atque perferendis enim ducimus? A exercitationem
-                      recusandae aliquam quod. Itaque inventore obcaecati, unde
-                      quam impedit praesentium veritatis quis beatae ea atque
-                      perferendis voluptates velit architecto?
-                    </p>
-                    <h4>$495.00</h4>
+                    <p>{{productDetails.description}}</p>
+                    <h4>${{productDetails.price}}</h4>
                   </div>
                   <div class="quantity">
                     <router-link to="/shopping-cart" class="primary-btn pd-cart"
@@ -137,6 +116,8 @@ import carousel from "vue-owl-carousel";
 import RelatedProductsSection from "@/components/RelatedProductsSection.vue";
 import Footer from "@/components/Footer.vue";
 
+import axios from "axios";
+
 export default {
   name: "Product",
   components: {
@@ -155,15 +136,24 @@ export default {
         "img/products/jacket/lyle-scott-jacket-3.jpg",
         "img/products/jacket/lyle-scott-jacket-4.jpg",
       ],
-      idProduct: this.$route.params.id
+      productDetails: []
     };
   },
   methods: {
     changeImage(srcImage) {
       this.mainImage = srcImage;
-      // eslint-disable-next-line no-console
-      console.log(this.idProduct);
     },
+  },
+  mounted() {
+    axios
+      .get("http://127.0.0.1:8000/api/product", {
+        params: {
+          id: this.$route.params.id
+        }
+      })
+      .then((res) => (this.productDetails = res.data.data))
+      // eslint-disable-next-line no-console
+      .catch((err) => console.log(err));
   },
 };
 </script>
